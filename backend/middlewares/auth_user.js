1d622c57030d4a02e8ca22b285e3a479eb8ba3a9
noama91 <noamash91@gmail.com>
2023-05-04 21:30:29 +0300

    auth middleware and generate token util

---
+++ backend/middlewares/auth_user.js
@@ -0,0 +1,19 @@
+const authenticate = (req, res, next) => {
+    const token = req.header('Authorization');
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(401).json({
+            message: 'Invalid token',
+            error: error.message
+        });
+    }
+}
+module.exports = authenticate;
---
+++ backend/utils/generate_token.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (user) => {
+
+    const payload = {
+        id: user._id,
+        username: user.username,
+        role: user.role
+    };
+
+    const options = {
+        expiresIn: process.env.JWT_EXPIRE
+    };
+
+    return jwt.sign(payload, process.env.JWT_SECRET, options);
+}
+
+module.exports = generateToken;
+
