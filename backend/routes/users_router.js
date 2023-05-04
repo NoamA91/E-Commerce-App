719229892a82ffd224d5ee440b6044214392bb49
noama91 <noamash91@gmail.com>
2023-05-04 21:26:17 +0300

    auth middleware

--- backend/routes/users_router.js
+++ backend/routes/users_router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const authenticate = require('../middlewares/authenticate');
 
 const {
-  registerUser
+  registerUser,
+  loginUser
 } = require('../controllers/users_controller');
 
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
