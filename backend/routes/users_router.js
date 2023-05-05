0cfc8a78def1f3403d4bb54d0488977725e3c8a2
NoamA91 <noamash91@gmail.com>
2023-05-05 21:12:59 +0300

    auth middlewares

--- backend/routes/users_router.js
+++ backend/routes/users_router.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userAuth = require('../middlewares/auth_user');
+const userAuth = require("../middlewares/auth_user");
+const adminAuth = require("../middlewares/auth_admin");
+const superAdminAuth = require("../middlewares/auth_superAdmin");
 
 const {
   registerUser,
   loginUser,
-  updateById
-} = require('../controllers/users_controller');
+  updateById,
+} = require("../controllers/users_controller");
 
-
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.put('/update/:id', userAuth, updateById)
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.put("/update/:id", userAuth, updateById);
 
 module.exports = router;
