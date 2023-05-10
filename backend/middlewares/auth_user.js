2377e0427a982599b913bba48986c4b735ab7230
noama91 <noamash91@gmail.com>
2023-05-10 22:11:27 +0300

    something

--- backend/middlewares/auth_user.js
+++ backend/middlewares/auth_user.js
@@ -1,23 +1,22 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
-const userAuth = (req, res, next) => {
-  const token = req.header("Authorization");
-  try {
-    if (!token) {
-      throw new Error("Invalid token");
+const userAuth = async (req, res, next) => {
+    const token = req.header("Authorization");
+    try {
+        if (!token) {
+            throw new Error("Invalid token");
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+
+        next();
+    } catch (error) {
+        res.status(401).json({
+            message: "Access denied",
+            error: error.message,
+        });
     }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.user = decoded;
-
-    next();
-  } catch (error) {
-    res.status(401).json({
-      message: "Access denied",
-      error: error.message,
-    });
-  }
 };
 module.exports = userAuth;
