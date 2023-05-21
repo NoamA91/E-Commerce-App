b0d29c04ccefcc9deafd70bbb48568771da62196
NoamA91 <noamash91@gmail.com>
2023-05-21 20:34:35 +0300

    admins_controller - added all functions

--- backend/controllers/admins_controller.js
+++ backend/controllers/admins_controller.js
@@ -1,6 +1,5 @@
 const User = require("../models/User");
 const colors = require("colors");
-const generateToken = require("../utils/generate_token");
 
 colors.setTheme({
   new_request: "magenta",
@@ -103,22 +102,213 @@ module.exports = {
     }
   },
   deleteManagerByIdForAdmin: async (req, res) => {
-    /* code here */
+    console.log("API DELETE request : delete manager by id".new_request);
+    try {
+      const managerId = req.params.id;
+
+      if (!managerId) {
+        throw new Error("Manager id is required");
+      }
+
+      console.log("manager id provided".step_done);
+
+      const manager = await User.findById(managerId);
+
+      if (!manager || manager.role !== "manager") {
+        throw new Error("Manager not found");
+      }
+
+      console.log("manager found".success_request);
+
+      await User.findByIdAndDelete(managerId);
+
+      console.log("manager deleted".success_request);
+
+      return res.status(200).json({
+        success: true,
+        message: "Manager deleted",
+      });
+    } catch (error) {
+      console.log(("error in delete manager by id request : " + error).failed_request);
+      return res.status(500).json({
+        message: "Error in delete manager by id request",
+        error: error.message,
+      });
+    }
   },
   getAllManagersForAdmin: async (req, res) => {
-    /* code here */
+    console.log("API GET request : get all managers".new_request);
+    try {
+      const managers = await User.find({ role: "manager" });
+
+      if (!managers) {
+        throw new Error("No managers found");
+      }
+
+      console.log("managers found".success_request);
+
+      return res.status(200).json({
+        success: true,
+        message: "Managers retrieved successfully",
+        managers,
+      });
+    } catch (error) {
+      console.log(("error in get all managers request : " + error).failed_request);
+      return res.status(500).json({
+        message: "Error in get all managers request",
+        error: error.message,
+      });
+    }
   },
 
   addUserForAdmin: async (req, res) => {
-    /* code here */
+    console.log("API POST request : add user".new_request);
+    try {
+      const { username, email, password } = req.body;
+
+      if (!username || !email || !password) {
+        throw new Error("Username, email and password are required");
+      }
+
+      console.log("username, email and password provided".step_done);
+
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        throw new Error("Email already exists");
+      }
+
+      const newUser = new User({
+        username,
+        email,
+        password,
+        role: "user",
+      });
+
+      const user = await newUser.save();
+
+      console.log("User added".success_request);
+
+      return res.status(201).json({
+        success: true,
+        message: "User added successfully",
+        user,
+      });
+    } catch (error) {
+      console.log(("error in add user request : " + error).failed_request);
+      return res.status(500).json({
+        message: "Error in add user request",
+        error: error.message,
+      });
+    }
   },
   updateUserByIdForAdmin: async (req, res) => {
-    /* code here */
+    console.log("API PUT request : update user by id".new_request);
+    try {
+      const userId = req.params.id;
+      const { username, email } = req.body;
+
+      if (!userId) {
+        throw new Error("User id is required");
+      }
+
+      console.log("user id provided".step_done);
+
+      const user = await User.findById(userId);
+
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      console.log("user found".step_done);
+
+      const updatedFields = {};
+
+      if (username) {
+        updatedFields.username = username;
+      }
+
+      if (email) {
+        updatedFields.email = email;
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(userId, updatedFields, {
+        new: true,
+        runValidators: true,
+      });
+
+      console.log("user updated".success_request);
+
+      return res.status(200).json({
+        success: true,
+        message: "User updated",
+        updatedUser,
+      });
+    } catch (error) {
+      console.log(("error in update user by id request : " + error).failed_request);
+      return res.status(500).json({
+        message: "Error in update user by id request",
+        error: error.message,
+      });
+    }
   },
   deleteUserByIdForAdmin: async (req, res) => {
-    /* code here */
+    console.log("API DELETE request : delete user by id".new_request);
+    try {
+      const userId = req.params.id;
+
+      if (!userId) {
+        throw new Error("User id is required");
+      }
+
+      console.log("user id provided".step_done);
+
+      const user = await User.findById(userId);
+
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      console.log("user found".step_done);
+
+      await User.findByIdAndDelete(userId);
+
+      console.log("user deleted".success_request);
+
+      return res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
+      });
+    } catch (error) {
+      console.log(("error in delete user by id request : " + error).failed_request);
+      return res.status(500).json({
+        message: "Error in delete user by id request",
+        error: error.message,
+      });
+    }
   },
   getAllUsersForAdmin: async (req, res) => {
-    /* code here */
+    console.log("API GET request : get all users".new_request);
+    try {
+      const users = await User.find({});
+
+      if (!users || users.length === 0) {
+        throw new Error("No users found");
+      }
+
+      console.log("Users retrieved".success_request);
+
+      return res.status(200).json({
+        success: true,
+        message: "Users retrieved successfully",
+        users,
+      });
+    } catch (error) {
+      console.log(("error in get all users request : " + error).failed_request);
+      return res.status(500).json({
+        message: "Error in get all users request",
+        error: error.message,
+      });
+    }
   },
 };
