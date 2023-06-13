0c4064acc83d8e25d562b928c76139cd04ce4f65
NoamA91 <noamash91@gmail.com>
2023-06-13 06:38:51 +0300

    managers - added routes and changed the links in sidebar

---
+++ client/Managers/src/components/sections/Sidebar/NavItem.jsx
@@ -0,0 +1,61 @@
+import { Flex, Text, Icon, Menu, MenuButton, Tooltip } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+const NavItem = ({ navSize, title, icon, active, handleClick, to }) => {
+  return (
+    <Flex
+      mt={30}
+      flexDir='column'
+      w='100%'
+      alignItems={navSize === "small" ? "center" : "flex-start"}
+    >
+      <Menu placement='right'>
+        {navSize === "small" ? (
+          <Tooltip borderRadius={5} hasArrow label={title} placement='right'>
+            <Link to={to} style={{ textDecoration: "none" }} onClick={() => handleClick(title)}>
+              <MenuButton
+                backgroundColor={active ? "#AEC8CA" : "transparent"}
+                borderRadius={8}
+                _hover={{ backgroundColor: "#AEC8CA" }}
+                w={navSize === "large" && "100%"}
+              >
+                <Flex padding={3}>
+                  <Icon as={icon} fontSize='xl' color={active ? "A82AAAD" : "grey.500"} />
+                  <Text
+                    ml={5}
+                    display={navSize === "small" ? "none" : "flex"}
+                    fontWeight={active ? "bold" : "normal"}
+                  >
+                    {title}
+                  </Text>
+                </Flex>
+              </MenuButton>
+            </Link>
+          </Tooltip>
+        ) : (
+          <Link to={to} style={{ textDecoration: "none" }} onClick={() => handleClick(title)}>
+            <MenuButton
+              backgroundColor={active ? "#AEC8CA" : "transparent"}
+              borderRadius={8}
+              _hover={{ backgroundColor: "#AEC8CA" }}
+              w={navSize === "large" && "180px"}
+            >
+              <Flex padding={3}>
+                <Icon as={icon} fontSize='xl' color={active ? "A82AAAD" : "grey.500"} />
+                <Text
+                  ml={5}
+                  display={navSize === "small" ? "none" : "flex"}
+                  fontWeight={active ? "bold" : "normal"}
+                >
+                  {title}
+                </Text>
+              </Flex>
+            </MenuButton>
+          </Link>
+        )}
+      </Menu>
+    </Flex>
+  );
+};
+
+export default NavItem;
---
+++ client/Managers/src/components/sections/Sidebar/Sidebar.jsx
@@ -0,0 +1,93 @@
+import { Flex, Divider, Avatar, Heading, Text, IconButton } from "@chakra-ui/react";
+import { useState } from "react";
+import { FiMenu, FiHome, FiUsers, FiBox, FiBook } from "react-icons/fi";
+import NavItem from "./NavItem";
+
+const Sidebar = () => {
+  const [navSize, setNavSize] = useState("large");
+  const [activeItem, setActiveItem] = useState("Dashboard");
+
+  const handleItemClick = (title) => {
+    setActiveItem(title);
+  };
+
+  return (
+    <Flex
+      pos='sticky'
+      left='5'
+      h='95vh'
+      mt='2.5vh'
+      boxShadow='0 4px 12px 0 rgba(0,0,0,0.05)'
+      borderRadius={navSize === "small" ? "15" : "30px"}
+      w={navSize === "small" ? "75px" : "200px"}
+      flexDir='column'
+      justifyContent='space-between'
+    >
+      <Flex
+        p='5%'
+        flexDir='column'
+        alignItems={navSize === "small" ? "center" : "flex-start"}
+        as='nav'
+      >
+        <IconButton
+          background='none'
+          mt={5}
+          _hover={{ background: "none" }}
+          icon={<FiMenu />}
+          onClick={() => {
+            navSize === "small" ? setNavSize("large") : setNavSize("small");
+          }}
+        />
+        <NavItem
+          navSize={navSize}
+          icon={FiHome}
+          title='Dashboard'
+          active={activeItem === "Dashboard"}
+          handleClick={handleItemClick}
+          to='/dashboard'
+        />
+        <NavItem
+          navSize={navSize}
+          icon={FiUsers}
+          title='Users'
+          active={activeItem === "Users"}
+          handleClick={handleItemClick}
+        />
+        <NavItem
+          navSize={navSize}
+          icon={FiBox}
+          title='Products'
+          active={activeItem === "Products"}
+          handleClick={handleItemClick}
+        />
+        <NavItem
+          navSize={navSize}
+          icon={FiBook}
+          title='Orders'
+          active={activeItem === "Orders"}
+          handleClick={handleItemClick}
+        />
+      </Flex>
+      <Flex
+        p='5%'
+        flexDir='column'
+        w='100%'
+        alignItems={navSize === "small" ? "center" : "flex-start"}
+        mb={4}
+      >
+        <Divider display={navSize === "small" ? "none" : "flex"} />
+        <Flex mt={4} alignItems='center'>
+          <Avatar size='sm' />
+          <Flex flexDir='column' ml={4} display={navSize === "small" ? "none" : "flex"}>
+            <Heading as='h3' size='sm'>
+              User Name
+            </Heading>
+            <Text color='grey'>Role</Text>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Sidebar;
