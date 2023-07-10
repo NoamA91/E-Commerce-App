57f91b39a9ff5e5f0d56ead1b68ca667567aa4f9
noama91 <noamash91@gmail.com>
2023-07-10 17:37:40 +0300

    Customers - Start

---
+++ client/Customers/src/components/sections/Nav.jsx
@@ -0,0 +1,13 @@
+import { Box, Flex } from "@chakra-ui/react"
+
+const Nav = () => {
+    return (
+        <>
+            <Flex>
+                <Box as='nav' w='100%' h='100px' bg='blackAlpha.900' ></Box>
+            </Flex>
+        </>
+    )
+}
+
+export default Nav
--- client/Customers/src/main.jsx
+++ client/Customers/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { extendTheme } from '@chakra-ui/react'
 import App from './App.jsx'
-import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+
+const colors = {
+  ShopYellow: '#fefed5',
+  ShopTeal: {
+    100: '#5edac8',
+    200: '#35d0ba',
+    300: '#28aa98',
+  },
+}
+
+const theme = extendTheme({ colors })
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ChakraProvider>
 )
