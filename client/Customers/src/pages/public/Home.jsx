d2c0baead4fce0670a9259849a4225cd9cf7d84e
noama91 <noamash91@gmail.com>
2023-07-15 21:14:23 +0300

    Customers - home page continue

--- client/Customers/src/pages/public/Home.jsx
+++ client/Customers/src/pages/public/Home.jsx
@@ -1,4 +1,5 @@
-import { Box, Heading, Text, Image, Flex, position, Circle } from '@chakra-ui/react';
+import * as url from 'url';
+import { Box, Heading, Text, Image, Flex, position, Circle, Grid, GridItem, Button } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { Parallax, ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax';
 
@@ -6,7 +7,6 @@ const Home = () => {
     return (
         <motion.div
             style={{
-                minHeight: '100vh',
                 width: '100%'
             }}
             initial={{ opacity: 0 }}
@@ -29,24 +29,26 @@ const Home = () => {
                     aspectRatio: '2 / 1.2',
                     height: '100vh',
                 }}>
-                    <ParallaxBannerLayer image="/2.png" speed={-30} />
+
+                    <ParallaxBannerLayer image="/2.png" speed={-40} />
+
                     <ParallaxBannerLayer>
-                        <Flex>
-                            <Box>
+                        <Flex
+                            flexDir={{ base: 'column', md: 'row' }}
+                        >
+                            <Box
+                                mt={{ base: '1rem', md: '10rem' }}
+                                ml={{ base: '1rem', md: '10rem' }}
+                            >
                                 <motion.div
                                     style={{
-                                        // width: '50%',
                                         minWidth: '300px',
-                                        minHeight: '30%',
-                                        backgroundColor: 'RGBA(0, 0, 0, 0.92)',
-                                        padding: '2em',
-                                        marginTop: '60px',
-                                        borderRadius: '0 15px 15px 0',
-                                        boxShadow: '0px 0px 15px 0px rgba(0, 0, 0, 0.75)',
+                                        padding: '2rem',
                                     }}
-                                    initial={{ width: 0 }}
+                                    initial={{ opacity: 0 }}
                                     animate={{
-                                        width: '60%',
+                                        opacity: 1,
+                                        transition: { duration: 0.6, delay: 0.1 }
                                     }}
                                 >
                                     <Heading
@@ -54,35 +56,130 @@ const Home = () => {
                                         size={{ base: 'xl', md: '3xl' }}
                                         color='ShopYellow'
                                     >
-                                        Online Shopping <br /> Made Easy
+                                        Online Shopping <br /> Make Life Easier
                                     </Heading>
                                     <Text
                                         mt={4}
                                         color='gray.200'
+
                                         fontSize={{ base: 'md', md: 'lg' }}
                                         lineHeight='2rem'
                                     >
                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, facere doloremque ad asperiores, sit distinctio odit ea debitis adipisci fuga recusandae dolores, ex impedit quas reiciendis! Et commodi atque aperiam.
                                     </Text>
+                                    <Button
+                                        mt={10}
+                                        bg='ShopYellow'
+                                        color='black'
+                                        _hover={{
+                                            bg: 'ShopTeal.100',
+                                        }}
+                                    >
+                                        Shop Now
+                                    </Button>
                                 </motion.div>
                             </Box>
+
                             <Box>
-                                <motion.div
-                                    style={{
-                                        width: '400px',
-                                        height: '100px',
-                                        background: 'center / cover no-repeat url(/welcome.png)',
-                                    }}
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: 1 }}
+                                <Grid
+                                    templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(2, 1fr)" }}
+                                    templateRows={{ base: "repeat(2, 1fr)", md: "repeat(2, 1fr)" }}
+                                    gap={4}
+                                    m={{ base: '1rem', md: '7rem' }}
                                 >
-                                    {/* <Image src='/welcome.png' alt='welcome' objectFit="cover" /> */}
-                                </motion.div>
+                                    <GridItem
+                                        minW={{ base: '150px', md: '250px' }}
+                                        maxW='300px'
+                                        height={{ base: '150px', md: '250px' }}
+                                    >
+                                        <motion.div
+                                            style={{
+                                                boxShadow: '-12px -10px 15px 9px rgba(0, 0, 0, 0.75)',
+                                                backgroundImage: 'url(/welcome.png)',
+                                                backgroundSize: 'cover',
+                                                borderRadius: '15px',
+                                                height: '100%',
+                                            }}
+                                            initial={{ opacity: 0 }}
+                                            animate={{
+                                                scale: [0, 1],
+                                                opacity: 1,
+                                                transition: { duration: 0.7, delay: 0.2 }
+                                            }}
+                                        >
+                                        </motion.div>
+                                    </GridItem>
+                                    <GridItem
+                                        minW={{ base: '150px', md: '250px' }}
+                                        maxW='300px'
+                                        height={{ base: '150px', md: '250px' }}
+                                    >
+                                        <motion.div
+                                            style={{
+                                                boxShadow: '12px -10px 15px 9px rgba(0, 0, 0, 0.75)',
+                                                backgroundImage: 'url(/phone.jpeg)',
+                                                backgroundSize: 'cover',
+                                                borderRadius: '15px',
+                                                height: '100%',
+                                            }}
+                                            initial={{ opacity: 0 }}
+                                            animate={{
+                                                scale: [0, 1],
+                                                opacity: 1,
+                                                transition: { duration: 0.7, delay: 0.4 }
+                                            }}
+                                        >
+                                        </motion.div>
+                                    </GridItem>
+                                    <GridItem
+                                        minW={{ base: '150px', md: '250px' }}
+                                        maxW='300px'
+                                        height={{ base: '150px', md: '250px' }}
+                                    >
+                                        <motion.div
+                                            style={{
+                                                boxShadow: '-12px 10px 15px 9px rgba(0, 0, 0, 0.75)',
+                                                backgroundImage: 'url(/AnimalsCart.jpeg)',
+                                                backgroundSize: 'cover',
+                                                borderRadius: '15px',
+                                                height: '100%',
+                                            }}
+                                            initial={{ opacity: 0 }}
+                                            animate={{
+                                                scale: [0, 1],
+                                                opacity: 1,
+                                                transition: { duration: 0.7, delay: 0.6 }
+                                            }}
+                                        >
+                                        </motion.div>
+                                    </GridItem>
+                                    <GridItem
+                                        minW={{ base: '150px', md: '250px' }}
+                                        maxW='300px'
+                                        height={{ base: '150px', md: '250px' }}
+                                    >
+                                        <motion.div
+                                            style={{
+                                                boxShadow: '12px 10px 15px 9px rgba(0, 0, 0, 0.75)',
+                                                backgroundImage: 'url(/Playing.png)',
+                                                backgroundSize: 'cover',
+                                                borderRadius: '15px',
+                                                height: '100%',
+                                            }}
+                                            initial={{ opacity: 0 }}
+                                            animate={{
+                                                scale: [0, 1],
+                                                opacity: 1,
+                                                transition: { duration: 0.7, delay: 0.8 }
+                                            }}
+                                        >
+                                        </motion.div>
+                                    </GridItem>
+                                </Grid>
                             </Box>
                         </Flex>
                     </ParallaxBannerLayer>
                 </ParallaxBanner>
-
 
             </Parallax>
         </motion.div >
