3c922aa1f1649c5ed9edfc469ba3044d7a498562
noama91 <noamash91@gmail.com>
2023-08-06 15:09:29 +0300

    Customers - added not found page

--- client/Customers/src/pages/public/NotFound.jsx
+++ client/Customers/src/pages/public/NotFound.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Heading, Text, Button, Center, VStack, ButtonGroup, Image } from '@chakra-ui/react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div>NotFound</div>
+        <Center h="80vh" flexDirection="column" bgColor="gray.100">
+            <VStack spacing={4}>
+                <Image
+                    w={150}
+                    src='/fox.png'
+                    alt='No products found'
+                />
+                <Heading color="red.500" size="4xl">404</Heading>
+                <Text fontSize="xl" fontWeight="medium">Oops! Page Not Found</Text>
+                <ButtonGroup>
+                    <Link onClick={() => navigate(-1)}>
+                        <Button colorScheme="red" >Back</Button>
+                    </Link>
+                    <Link to='/'>
+                        <Button colorScheme="red" >Go Home</Button>
+                    </Link>
+                </ButtonGroup>
+            </VStack>
+        </Center>
     )
 }
 
-export default NotFound
+export default NotFound;
