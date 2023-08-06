735ee9f6c0edca41718a236ccfb7fe6b51dbbb08
NoamA91 <noamash91@gmail.com>
2023-08-06 06:04:26 +0300

    Backend - Fixed bug with removing old tokens

--- backend/controllers/users_controller.js
+++ backend/controllers/users_controller.js
@@ -114,9 +114,7 @@ module.exports = {
       if (oldTokens.length) {
         oldTokens = oldTokens.filter((t) => {
           const timeDiff = (currentTime - parseInt(t.signedAt)) / 1000;
-          if (timeDiff <= 10800000) {
-            return t;
-          }
+          return timeDiff <= 10800;
         });
       }
 
